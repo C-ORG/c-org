9389e789d70ba61e0b0e14b1eedde0243fe84589
Tibor Vass <teabee89@gmail.com>
2018-10-06 15:23:42 -0700

    ui: add Sidebar skeleton
    Signed-off-by: Tibor Vass <teabee89@gmail.com>

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import {Sidebar} from "./Components";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+	<Sidebar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>

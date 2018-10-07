86e056c131431eb65c7afd99d039ed0fbecdb0b8
Andrea Luzzardi <aluzzardi@gmail.com>
2018-10-06 21:18:09 -0700

    contracts: set beta to 50
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ module.exports = function(deployer) {
   const symbol = "TST";
   const slope = 1000; // parametrize the buying linear curve
   const alpha = 100; // fraction put into selling reserves when investors buy
-  const beta = 300; // fraction put into selling reserves when the CO has revenues
+  const beta = 50; // fraction put into selling reserves when the CO has revenues
 
   deployer.deploy(ContinuousOrganization, name, symbol, slope, alpha, beta);
 };

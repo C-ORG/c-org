19ca8c2e3c49fa682593c72bf4ebce50a328d4ee
Andrea Luzzardi <aluzzardi@gmail.com>
2018-10-06 17:04:31 -0700

    contract: Implement ERC20
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- contracts/contracts/ContinuousOrganization.sol
+++ contracts/contracts/ContinuousOrganization.sol
@@ -2,9 +2,16 @@ pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
-contract ContinuousOrganization is Ownable {
+contract ContinuousOrganization is Ownable, StandardToken {
     using SafeMath for uint256;
+
+    // ERC20 Token Definition.
+    // Name and Symbol are not constants since we're going to set them at construction time.
+    string public name;
+    string public symbol;
+    uint8 public constant decimals = 18;
 
     /* The parameters of the Continuous Organisation. All are multiplied by 1000 */
     uint slope = 1000; // parametrize the buying linear curve
@@ -12,20 +19,21 @@ contract ContinuousOrganization is Ownable {
     uint beta = 300; // fraction put into selling reserves when the CO has revenues
 
     /* The tokens of the Continuous Organisation */
-    uint nTokens = 0;
-    mapping(address => uint) public balances;
-    uint public sellReserve = 0;
+    uint256 private sellReserve_ = 0;
 
     /* Events */
     event UpdateTokens(uint tokens, uint sellReserve);
 
     /* This is the constructor. Solidity does not implement float number so we have to multiply constants by 1000 and rounding them before creating the smart contract.
     */
-    constructor(uint paramA, uint paramAlpha, uint paramBeta) public {
-        require(paramAlpha < 1001 && paramBeta < 1001);
-        slope = paramA;
-        alpha = paramAlpha;
-        beta = paramBeta;
+    constructor(string _name, string _symbol, uint _slope, uint _alpha, uint _beta) public {
+        require(_alpha <= 1000 && _beta <= 1000);
+
+        name = _name;
+        symbol = _symbol;
+        slope = _slope;
+        alpha = _alpha;
+        beta = _beta;
     }
 
     /* Getters and setters */
@@ -38,25 +46,37 @@ contract ContinuousOrganization is Ownable {
     function setBeta(uint paramBeta) public onlyOwner {
         beta = paramBeta;
     }
-    function getBalance()
+    function sellReserve()
         public
         view
-        returns (uint y) {
-        y = balances[msg.sender];
-    }
-    function getSellReserve()
-        public
-        view
-        returns (uint y) {
-        y = sellReserve;
-    }
-    function getNumTokens()
-        public
-        view
-        returns (uint y) {
-        y = nTokens;
+        returns (uint256) {
+        return sellReserve_;
     }
 
+    // From: https://github.com/OpenZeppelin/openzeppelin-solidity/blob/v1.12.0/contracts/token/ERC20/MintableToken.sol
+    function _mint(
+        address _to,
+        uint256 _amount
+    )
+        internal
+        returns (bool)
+    {
+        totalSupply_ = totalSupply_.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        emit Transfer(address(0), _to, _amount);
+        return true;
+    }
+
+    // From: https://github.com/OpenZeppelin/openzeppelin-solidity/blob/v1.12.0/contracts/token/ERC20/BurnableToken.sol
+    function _burn(address _who, uint256 _value) internal {
+        require(_value <= balances[_who]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+        balances[_who] = balances[_who].sub(_value);
+        totalSupply_ = totalSupply_.sub(_value);
+        emit Transfer(_who, address(0), _value);
+    }
 
     /* Babylonian method for square root. See: https://ethereum.stackexchange.com/a/2913 */
     function _sqrt(uint x)
@@ -78,29 +98,31 @@ contract ContinuousOrganization is Ownable {
 
         // create tokens
         uint invest = msg.value;
-        uint tokens = _sqrt(2*invest*1000/slope + nTokens*nTokens) - nTokens;
-        balances[msg.sender] += tokens;
-        nTokens += tokens;
+        uint tokens = _sqrt(2*invest*1000/slope + totalSupply_*totalSupply_) - totalSupply_;
+        _mint(msg.sender, tokens);
 
-        // redistribute tokens
-        sellReserve += alpha*invest;
-        owner.transfer((1000-alpha)/1000*invest);
+        // redistribute tokens: keep a fraction in reserve and transfer the rest.
+        uint reserve = alpha*invest;
+        sellReserve_ += reserve;
+        owner.transfer(invest - reserve);
 
-        emit UpdateTokens(nTokens, sellReserve);
+        emit UpdateTokens(totalSupply_, sellReserve_);
     }
 
     function sell(uint tokens) public {
         // check funds
         require(tokens > 0);
-        require(balances[msg.sender] >= tokens);
 
-        balances[msg.sender] -= tokens;
-        uint withdraw = sellReserve*tokens/nTokens/nTokens*(2*nTokens - tokens);
-        sellReserve -= withdraw;
+        // Burn the deposit. This will check the seller has enough.
+        _burn(msg.sender, tokens);
+
+        // Pay out from the reserve.
+        uint withdraw = sellReserve_*tokens/totalSupply_/totalSupply_*(2*totalSupply_ - tokens);
+        sellReserve_ -= withdraw;
         withdraw /= 1000;
         msg.sender.transfer(withdraw);
 
-        emit UpdateTokens(nTokens, sellReserve);
+        emit UpdateTokens(totalSupply_, sellReserve_);
     }
 
     function revenue()
@@ -111,22 +133,13 @@ contract ContinuousOrganization is Ownable {
         require(msg.value > 0);
         uint rev = msg.value;
         // create tokens
-        uint tokens = _sqrt(2*rev*1000/slope + nTokens*nTokens) - nTokens;
-        balances[owner] += tokens;
-        nTokens += tokens;
+        uint tokens = _sqrt(2*rev*1000/slope + totalSupply_*totalSupply_) - totalSupply_;
+        _mint(owner, tokens);
 
         // redistribute tokens
-        sellReserve += beta*rev;
+        sellReserve_ += beta*rev;
         owner.transfer((1-beta)*rev/1000);
 
-        emit UpdateTokens(nTokens, sellReserve);
-    }
-
-    function freeTokens(uint amount)
-        public
-        onlyOwner {
-
-        require(amount > 0);
-        nTokens += amount;
+        emit UpdateTokens(totalSupply_, sellReserve_);
     }
 }
--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 let ContinuousOrganization = artifacts.require("./ContinuousOrganization.sol");
 
 module.exports = function(deployer) {
+  const name = "test token";
+  const symbol = "TST";
   const slope = 1000; // parametrize the buying linear curve
   const alpha = 100; // fraction put into selling reserves when investors buy
   const beta = 300; // fraction put into selling reserves when the CO has revenues
 
-  deployer.deploy(ContinuousOrganization, slope, alpha, beta);
+  deployer.deploy(ContinuousOrganization, name, symbol, slope, alpha, beta);
 };

c9d10b35396e0dd7495c2bee6837f18565172a4f
Tibor Vass <teabee89@gmail.com>
2018-10-07 01:08:51 -0700

    toto
    Signed-off-by: Tibor Vass <teabee89@gmail.com>

--- contracts/migrations/1_initial_migration.js
+++ contracts/migrations/1_initial_migration.js
@@ -1,5 +1,28 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
-module.exports = function(deployer) {
+/*
+const checkBalance = account => new Promise((resolve, reject) => {
+  web3.eth.getBalance(account, (err, result) => {
+    
+    if (err) {
+      return reject(err);
+    }
+
+    resolve(result.toNumber());
+  });
+});
+*/
+module.exports = function(deployer, network, accounts) {
   deployer.deploy(Migrations);
+/*
+  return checkBalance(accounts[0])
+    .then(accountBalance => {
+
+      console.log('Current account balance', accountBalance);
+
+      return Promise.resolve();
+    })
+    .then(_ => deployer.deploy(Migrations))
+    .catch(err => throw err);
+*/
 };
--- contracts/truffle.js
+++ contracts/truffle.js
@@ -1,10 +1,18 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider("man sleep dance broom creek invite fragile wisdom divorce loan three split", "https://ropsten.infura.io/b26c78e15a68424294d4ebb9614261c5b26c78e15a68424294d4ebb9614261c5");
+      },
+      network_id: '3',
+    },
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*"
     }
   }
